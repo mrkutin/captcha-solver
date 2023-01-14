a8bf62f2d688a41ff608b76d611725077a4b06c7
Mr. Kutin <serge.kutin@gmail.com>
2023-01-14 09:59:39 -0500

    auth

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from fastapi import Request, FastAPI
+from fastapi import Request, FastAPI, HTTPException
 from solve_captcha import solve_base64
 
 app = FastAPI()
 
 
 @app.get('/')
-def read_root(request: Request):
-    return {'OK'}
+async def read_root(request: Request):
+    return 'OK'
 
 
 @app.post('/solve')
 async def read_root(request: Request):
+    if(request.headers['authorization'] != 'Bearer s6Y!zLDz18bFUSYm9O7%5$ZDu%E4BhFrArGptTBW8XSOx2G2GcOqk*&YLxkgY8rnILBj0&!4oc9mrojs%vhFDIHg7z!xQz%B&JgsUi5DUpSuZSxh7VY#0H54nTO6yDBC'):
+        raise HTTPException(status_code=401, detail='Unauthorized')
     body = await request.body()
     solution = solve_base64(body)
     return solution
